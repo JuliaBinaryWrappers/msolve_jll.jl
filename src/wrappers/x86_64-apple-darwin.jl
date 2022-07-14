949f4654700088bdfb54e4151ca0c9638a005177
jlbuild <juliabuildbot@gmail.com>
2022-07-14 21:52:41 +0000

    msolve_jll build 0.4.0+0

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -5,8 +5,8 @@ using GMP_jll
 using FLINT_jll
 using LLVMOpenMP_jll
 JLLWrappers.@generate_wrapper_header("msolve")
-JLLWrappers.@declare_library_product(libmsolve, "@rpath/libmsolve-0.3.0.dylib")
-JLLWrappers.@declare_library_product(libneogb, "@rpath/libneogb-0.3.0.dylib")
+JLLWrappers.@declare_library_product(libmsolve, "@rpath/libmsolve-0.4.0.dylib")
+JLLWrappers.@declare_library_product(libneogb, "@rpath/libneogb-0.4.0.dylib")
 function __init__()
     JLLWrappers.@generate_init_header(GMP_jll, FLINT_jll, LLVMOpenMP_jll)
     JLLWrappers.@init_library_product(
