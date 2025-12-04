9d1e408cd1c9e2331dc66afcdc9faabbc3151f5f
jlbuild <juliabuildbot@gmail.com>
2025-12-04 10:54:01 +0000

    msolve_jll build 0.900.300+0

--- src/msolve_jll.jl
+++ src/msolve_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("msolve")
-JLLWrappers.@generate_main_file("msolve", UUID("6d01cc9a-e8f6-580e-8c54-544227e08205"))
+JLLWrappers.@generate_main_file("msolve", Base.UUID("6d01cc9a-e8f6-580e-8c54-544227e08205"))
 end  # module msolve_jll
